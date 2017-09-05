45af76d664bfb525275d1f95fda757b50c79061e
fluorescent-net <leavemydog@gmail.com>
2017-09-05 19:44:26 +0300

    Probably fix travis database access.

--- eris_hub/settings/travis.py
+++ eris_hub/settings/travis.py
@@ -4,7 +4,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'eris_test',
-        'USER': 'travis',
+        'USER': 'root',
         'HOST': 'localhost',
         'PORT': '3306',
     }
